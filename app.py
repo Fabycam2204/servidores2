1ad971a4252b04deb94b8308c5650ca0414ce8b6
appsatenea_bog@unal.edu.co <appsatenea_bog@unal.edu.co>
2024-08-27 11:53:59 -0500

    Terminada página de inicio

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ servidor2 = Flask(__name__)
 
 @servidor2.route('/inicio')
 def home():
-    return render_template('plantilla.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     servidor2.run(debug=True)
